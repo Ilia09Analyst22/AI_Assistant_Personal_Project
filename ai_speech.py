d7f16b04bb00ef0eb83f33f8635c136beef05e76
irsam <irsamii@outlook.com>
2025-08-08 16:55:12 -0400

    feat(ai_speech): added logging to speak function

--- ai_speech.py
+++ ai_speech.py
@@ -14,13 +14,17 @@ def speak() -> bool:
         # Initialize voice engine
         engine = pyttsx3.init()
     except Exception as err:
-        pass
+        Logger.error(f"Failed to setup AI speech engine: {err}")
+        return False
+    Logger.info("AI speech setup successful")
 
     try:
         # Set the voice of the AI assistant
         engine.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0")
     except Exception as err:
-        pass
+        Logger.error(f"Failed to set the voice for the AI assistant: {err}")
+        return False
+    Logger.info("AI voice was set successfully")
 
     try:
         # Tell the assistant to speak
@@ -29,6 +33,10 @@ def speak() -> bool:
         # Run the interactive assistant
         engine.runAndWait()
     except Exception:
-        pass
+        Logger.error("AI speak function failed during runtime")
+        return False
+    
+    Logger.info("Successfully ran AI speech")
+    return True
 
 
