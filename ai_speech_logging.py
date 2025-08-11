02dc7de603bebd3ce60718b0f89c4fac90054725
irsam <irsamii@outlook.com>
2025-08-11 00:58:28 -0400

    feat(ai_speech_logging): added voice and text interact logging functions

--- ai_speech_logging.py
+++ ai_speech_logging.py
@@ -39,3 +39,39 @@ def chat_logging(chat_func: function) -> None:
         "the cause of the failure. It could be one of the following: The AI voice failed to setup " \
         "properly, The web browser failed to interact with ChatGPT, or an element on the ChatGPT " \
         "webpage was not found.")
+
+def voice_interact_logging(voice_func: function) -> None:
+    """A logging method for AI voice interaction testcase.
+    
+    Args:
+        voice_func: The voice interact function for testing.
+    """
+    log = Logger("AI voice interact logging")
+    if voice_func:
+        Logger.info("Successful voice interaction with AI assistant")
+        Logger.setLevel(log, "SUCCESS")
+    else:
+        Logger.error("Just to let you know, the AI interactive voice agent failed")
+        Logger.fatal("Voice interaction with AI assistant failed!!")
+        Logger.setLevel(log, "FAIL")
+        print("The testcase failed due to an issue with ChatGPT interaction and " \
+        "selenium web browsing functionality. Please check to make sure you have entered a valid " \
+        "voice ID and that you have internet access.")
+
+def text_interact_logging(text_func: function) -> None:
+    """A logging method for AI interactive text agent.
+    
+    Args:
+        voice_func: The text interact function from ai_speech.
+    """
+    log = Logger("AI text interact logging")
+    if text_func:
+        Logger.info("Successful text interaction with AI assistant")
+        Logger.setLevel(log, "SUCCESS")
+    else:
+        Logger.error("Just to let you know, the AI interactive text agent failed")
+        Logger.fatal("Text interaction with AI assistant failed!!")
+        Logger.setLevel(log, "FAIL")
+        print("The test failed due to an issue with selenium web browsing functionality. Please " \
+        "make sure you have a stable internet connection, or check for any invalid locator values " \
+        "or selector methods in the function.")
