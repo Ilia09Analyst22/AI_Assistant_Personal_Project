d941eac26ff052868caa0a177c491507c05ee8be
irsam <irsamii@outlook.com>
2025-08-12 15:54:47 -0400

    feat: added ai_speech_testing module

---
+++ ai_speech_testing.py
@@ -0,0 +1,10 @@
+"""Module for testing AI speech related functions."""
+
+# Required Libraries
+from unittest.case import TestCase as test
+from ai_speech import speak, chatbox, voice_interact, text_interact
+
+class TestAISpeech:
+    """Testbase for testing AI speech functions."""
+
+    pass
