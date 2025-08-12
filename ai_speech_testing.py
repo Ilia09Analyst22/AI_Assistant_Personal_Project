ad685fff6a07c09f007ffe6ec4e7dcdf7d2720cb
irsam <irsamii@outlook.com>
2025-08-12 16:03:53 -0400

    feat: added test_speak testcase to ai_speech_testing

--- ai_speech_testing.py
+++ ai_speech_testing.py
@@ -1,10 +1,16 @@
 """Module for testing AI speech related functions."""
 
 # Required Libraries
-from unittest.case import TestCase as test
+from unittest.case import TestCase
 from ai_speech import speak, chatbox, voice_interact, text_interact
 
-class TestAISpeech:
+class TestAISpeech(TestCase):
     """Testbase for testing AI speech functions."""
 
-    pass
+    def test_speak(self) -> bool:
+        """Testing the speak functionality of AI assistant.
+        
+        Returns:
+            True if AI voice can be heard, False otherwise.
+        """
+        self.assertTrue(speak())
