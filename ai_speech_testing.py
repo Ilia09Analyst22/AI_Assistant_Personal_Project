8995b15ca870c5b910c568c42d3815287b4c2c4e
irsam <irsamii@outlook.com>
2025-08-12 20:04:33 -0400

    feat: added testcases for AI text and voice interactive functions

--- ai_speech_testing.py
+++ ai_speech_testing.py
@@ -25,3 +25,28 @@ class TestAISpeech(TestCase):
         audio_or_text = random.choice(["audio", "text"])
         self.assertTrue(chatbox(audio_or_text))
 
+    def test_voice_interact(self) -> bool:
+        """Testing the interactive voice function of AI assistant.
+        
+        Returns:
+            True if voice interactive function runs successfully, False otherwise.
+        """
+        import random
+        num_reqs = random.choice(range(10))
+        voices = ["HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0",
+                  "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0",
+                  "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0",
+                  "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-ES_HELENA_11.0",
+                  "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0"]
+        voice_id = random.choice(voices)
+        self.assertTrue(voice_interact(voice_id, num_reqs))
+
+    def test_text_interact(self) -> bool:
+        """Testing the interactive test function of AI assistant.
+        
+        Returns:
+            Boolean value indicating success of interactive text functionality.
+        """
+        import random
+        num_reqs = random.choice(range(10))
+        self.assertTrue(text_interact(num_reqs))
