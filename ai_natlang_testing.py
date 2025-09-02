2aeaf6f530f058fe6bd03cd782ea0fb98118f011
irsam <irsamii@outlook.com>
2025-09-01 23:52:56 -0400

    fix: removed incorrect return statements in ai_natlang_testing

--- ai_natlang_testing.py
+++ ai_natlang_testing.py
@@ -13,7 +13,7 @@ class TestAINatLang(TestCase):
     """Method for testing AI NatLang features."""
 
     generic_greet = "Hello, how may I help you?"
-    def test_words_or_sentences(self, word_sent: str) -> bool:
+    def test_words_or_sentences(self) -> bool:
         """Test word or sentence tokenization on generic greeting.
         
         Args:
@@ -23,27 +23,29 @@ class TestAINatLang(TestCase):
         Returns:
             True if word or sentence tokenization was successful, False otherwise.
         """
-        return self.assertTrue(check_with_greeting(word_sent))
+        import random
+        word_sent = random.choice(["words", "sentences"])
+        self.assertTrue(check_with_greeting(word_sent))
     
     def test_pos(self):
         """Test POS feature of AI NatLang."""
-        return self.assertAlmostEqual(pos(self.generic_greet),[('Hello', 'NNP'), ('.', '.'), ('How', 'WRB'), ('can', 'MD'), ('I', 'PRP'), ('help', 'VB'), ('you', 'PRP'), ('?', '?')])
+        self.assertAlmostEqual(pos(self.generic_greet),[('Hello', 'NNP'), ('.', '.'), ('How', 'WRB'), ('can', 'MD'), ('I', 'PRP'), ('help', 'VB'), ('you', 'PRP'), ('?', '?')])
     
     def test_basic_chunk(self):
         """Test chunking with generic text."""
         some_text = "A great successful man"
-        return self.assertRegex(some_text, "NP: {<DT>?<JJ>*<NN>}")
+        self.assertRegex(some_text, "NP: {<DT>?<JJ>*<NN>}")
     
     def test_natlang_chunk(self):
         """Test chunk feature of AI NatLang."""
         import nltk.tree.tree as tr
         pattern = "NP: {<DT>?<JJ>*<NN>}"
-        return self.assertIsInstance(chunk(pos("A great successful man"),pattern), tr.Tree)
+        self.assertIsInstance(chunk(pos("A great successful man"),pattern), tr.Tree)
     
     def test_nl_process(self):
         """Test Natural Language Processing."""
         gg = self.generic_greet
-        return self.assertEqual(nl_processor(gg), ["Hello",",","how","may","I","help","you","?"])
+        self.assertEqual(nl_processor(gg), ["Hello",",","how","may","I","help","you","?"])
     
     def regex(self):
         """Test Regex and sentence tokenization."""
